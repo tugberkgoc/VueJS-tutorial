ed2e83b8802254048c41c7f3c8238bfc1abd6d28
tugberkgoc <tugberkgoc@gmail.com>
2019-01-21 13:17:47 +0300

    Finish Module 03 in nodeJS

--- nodejs/03-understanding-the-basics/app.js
+++ nodejs/03-understanding-the-basics/app.js
@@ -1,46 +1,13 @@
 const http = require('http');
-const fs = require('fs');
+const router = require('./routes')
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(router.handler
   // console.log(req.url, req.method, req.headers);
   // process.exit();
+  // const url = req.url;
+  // const method = req.method;
+);
 
-  const url = req.url;
-  const method = req.method;
-
-  if (url === '/') {
-    res.setHeader('Content-type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My first Page!!!</title></head>');
-    res.write('<body>' +
-        '<form action="/message" method="POST">' +
-        '<input type="text" name="message">' +
-        '<button type="submit">Send .' +
-        '</button></form></body>');
-    res.write('</html>');
-    return res.end();
-  } else if (url === '/message' && method === 'POST') {
-    const body = [];
-    req.on('data', (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      //console.log(parsedBody);
-      const message = parsedBody.split('=')[1];
-      fs.writeFileSync('message.txt', message);
-    });
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
-  }
-  res.setHeader('Content-type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>My first Page!!!</title></head>');
-  res.write('<body><h1>Hello from my nodeJs server.</h1></body>');
-  res.write('</html>');
-  res.end();
-});
+console.log(router.someText);
 
 server.listen(3000);
