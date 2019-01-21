f5ab7b1f535153ba1d8c19aba39073302cfcaeef
tugberkgoc <tugberkgoc@gmail.com>
2019-01-22 00:01:43 +0300

    Finish Module 05

---
+++ nodejs/05-express-js/routes/admin.js
@@ -0,0 +1,17 @@
+const path = require('path');
+// const rootDir = require('../util/path');
+const express = require('express');
+const router = express.Router();
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
+});
+
+// /admin/add-product => POST
+router.post('/add-product', (req, res) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+module.exports = router;
