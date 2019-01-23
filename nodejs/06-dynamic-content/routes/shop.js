54138e00da3098624e8c9c939f9f8a13a881db0a
tugberkgoc <tugberkgoc@gmail.com>
2019-01-23 21:50:56 +0300

    Finish Module 06 - Dynamic Content

---
+++ nodejs/06-dynamic-content/routes/shop.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const adminData = require('./admin')
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    activeAddProduct: false,
+    productCSS: true
+  });
+});
+
+module.exports = router;
--- vuejs/client/src/views/About.vue
+++ vuejs/client/src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>{{number}}</p>
   </div>
 </template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    data() {
+      return {
+        number: null
+      }
+    },
+    created() {
+      axios.get('http://localhost:3000/admin/add-product')
+        .then(a => this.number = a);
+    }
+  }
+</script>
