37e1122396fe264e6b93e3b5d2f430f49ed4a3a6
tugberkgoc <tugberkgoc@gmail.com>
2019-01-24 00:44:37 +0300

    :fire:Starts Module 07 - MVC

---
+++ nodejs/07-mvc/routes/shop.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const adminData = require('./admin')
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    activeAddProduct: false,
+    productCSS: true
+  });
+});
+
+module.exports = router;
